7b8b25b78c36d326c717689a17c0fbfa568e3251
Jimmy O'Hara <123116539+Jimby3@users.noreply.github.com>
2025-02-27 15:23:05 -0700

    init landing page

--- CS4300/CS4300_django_server/asgi.py
+++ CS4300/CS4300_django_server/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for CS4300 project.
+ASGI config for CS4300_django_server project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CS4300.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CS4300_django_server.settings')
 
 application = get_asgi_application()
