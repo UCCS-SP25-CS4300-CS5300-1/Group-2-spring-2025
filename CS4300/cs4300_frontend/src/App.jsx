625b139acec1815218ce3d560e400979c813b2b7
Jimmy O'Hara <123116539+Jimby3@users.noreply.github.com>
2025-02-27 16:03:54 -0700

    Basic react setup

---
+++ CS4300/cs4300_frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/homePage/home";
+import Navbar from "./components/navbar/navbar";
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
