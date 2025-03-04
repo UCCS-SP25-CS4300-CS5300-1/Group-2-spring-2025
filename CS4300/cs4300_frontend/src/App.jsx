26dd0df7fb64ca069c621248a49b9cde877f66c0
Jimmy O'Hara <123116539+Jimby3@users.noreply.github.com>
2025-03-04 13:37:26 -0700

    got gh-pages setup and finalized iter0-3

--- CS4300/cs4300_frontend/src/App.jsx
+++ CS4300/cs4300_frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/pages/homePage/home";
 import Navbar from "./components/navbar/navbar";
 
