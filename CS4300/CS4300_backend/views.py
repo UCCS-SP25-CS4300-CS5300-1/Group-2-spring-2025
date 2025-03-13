f2562dfb0781f6350f472d002a094a499fa6ad09
Jimmy O'Hara <123116539+Jimby3@users.noreply.github.com>
2025-03-13 14:54:51 -0600

    test coverage working

--- CS4300/CS4300_backend/views.py
+++ CS4300/CS4300_backend/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# File: CS4300/CS4300_backend/views.py
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class ExampleApiView(APIView):
+    def get(self, request):
+        return Response({'message': 'Hello, world!'})
