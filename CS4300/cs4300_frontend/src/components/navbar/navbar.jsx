3f9e593778c7d6eca4567a4ed3faf74e1de1a161
Styre-x <robotgarland@gmail.com>
2025-03-17 19:20:33 -0600

    fix router issues on gh-pages

--- CS4300/cs4300_frontend/src/components/navbar/navbar.jsx
+++ CS4300/cs4300_frontend/src/components/navbar/navbar.jsx
@@ -5,9 +5,9 @@ function Navbar() {
     return (
         <nav className="navbar">
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
+                <li><a href="/Group-2-spring-2025/">Home</a></li>
+                <li><a href="/Group-2-spring-2025/about">About</a></li>
+                <li><a href="/Group-2-spring-2025/contact">Contact</a></li>
             </ul>
         </nav>
     );
