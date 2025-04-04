0d71c44541e58a5c74dc2217fd8db36eaa974065
Jordan <jcamden@uccs.edu>
2025-04-04 00:17:44 -0600

    test

--- CS4300/CS4300_django_server/settings.py
+++ CS4300/CS4300_django_server/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
+this is a test
 """
 
 from pathlib import Path
