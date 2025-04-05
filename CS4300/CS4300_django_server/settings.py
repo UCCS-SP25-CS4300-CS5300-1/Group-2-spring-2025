d2481e1684afd7870cf1e90a4b69db8028f0bec8
Jordan <jcamden@uccs.edu>
2025-04-04 19:29:06 -0600

    removed test from settings.py

--- CS4300/CS4300_django_server/settings.py
+++ CS4300/CS4300_django_server/settings.py
@@ -8,7 +8,6 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
-this is a test
 """
 
 from pathlib import Path
