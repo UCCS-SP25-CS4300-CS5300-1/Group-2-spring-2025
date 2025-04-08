db4978b18e73057d0cacfc4c52f34233a1c873f2
Jimmy O'Hara <123116539+Jimby3@users.noreply.github.com>
2025-04-08 15:09:24 -0600

    test commit for demo

--- CS4300/cs4300_frontend/src/components/pages/contact/contact.jsx
+++ CS4300/cs4300_frontend/src/components/pages/contact/contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
         <p>
           Have questions, feedback, or need support? We'd love to hear from you!
         </p>
-        <p>Email: <a href="mailto:johara3@uccs.edu">group2@uccs.edu</a></p>
+        <p>Email: <a href="mailto:johara3@uccs.edu">group2@uccs.edu test</a></p>
         <p>GitHub: <a href="https://github.com/UCCS-SP25-CS4300-CS5300-1/Group-2-spring-2025" target="_blank" rel="noopener noreferrer">Group 2 Repo</a></p>
       </div>
     </div>
