048e58698c25aedcf3de9897dc20796fc620dbbf
Jenna O'Hara <jennagutier9@gmail.com>
2025-04-08 09:45:05 -0600

    fixed where .env was stored in gitignore file

--- CS4300/CS4300_backend/models.py
+++ CS4300/CS4300_backend/models.py
@@ -72,8 +72,8 @@ class Product:
         client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
         prompt = (
-            "Generate a health score, scaled from 1-10, 2 newlines, and a summary (75-100 words) of its health factors based on "
-            "this information about the food:"
+            "Generate a health score, scaled from 1-10, 2 newlines, and a summary (75-100 words) of its health factors "
+            "based on this information about the food:"
             f"Product name: {self.name}"
             f"Product Nutrition Data: {self.nutrition_data}"
         )
