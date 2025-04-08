4c3f53fdac7d32db7337d8a5d8a7d6d72aeace60
Jimmy O'Hara <123116539+Jimby3@users.noreply.github.com>
2025-04-07 18:21:59 -0600

    finally

--- CS4300/CS4300_backend/tests/test_base.py
+++
@@ -1,8 +0,0 @@
-from django.core.management import call_command
-from rest_framework.test import APITestCase
-
-class BaseTestCase(APITestCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        call_command('migrate', verbosity=0)
