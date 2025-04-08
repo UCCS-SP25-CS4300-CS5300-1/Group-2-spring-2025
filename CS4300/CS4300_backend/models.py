d7c9efef2ef9b3b412ed9e406ec54d8bcfbcc475
Jenna O'Hara <jennagutier9@gmail.com>
2025-04-08 08:35:23 -0600

    Updated health score prompt for shorter response

--- CS4300/CS4300_backend/models.py
+++ CS4300/CS4300_backend/models.py
@@ -72,8 +72,8 @@ class Product:
         client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
         prompt = (
-            "Generate a health score on a scale from 1-10 and a summary of its health factors based on this "
-            "information about the food:"
+            "Generate a health score, scaled from 1-10, and a summary (75-100 words) of its health factors based on "
+            "this information about the food:"
             f"Product name: {self.name}"
             f"Product Nutrition Data: {self.nutrition_data}"
         )
